9e3147f0d515940eea22ef14f681eda2bd5ebe23
Jorge <jorge@imatec.cl>
2014-04-30 17:56:00 -0400

    ble

--- app/Config/database.php
+++ app/Config/database.php
@@ -67,7 +67,7 @@ class DATABASE_CONFIG {
 		'host' => 'localhost',
 		//'login' => 'root',
 		'login' => 'buscaruca',
-		//'password' => 'zewYqJB3jwDqYRQx',
+		//'password' => '8cNASXXeWXdHsDzH',
 		'password' => '',
 		'database' => 'buscaruca',
 		'prefix' => '',
